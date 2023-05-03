64b02639a5b54599ec4c615ec94dc486ad931563
liuding <liu_ding@yahoo.com>
2023-05-03 09:02:23 +0800

    0503 init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h1>0503 </h1> 
+
+    <a href="./0503_01.php">1.01 </a><br>
+    <a href=".">2.</a><br>
+    <a href=".">3.</a> <br>
+    <a href=".">4.</a> <br>
+    
+</body>
+</html>
